4d9fcc9852d4b48f3d86be50a14841e9fb72a2c9
lambert-x <44768908+lambert-x@users.noreply.github.com>
2021-12-01 05:19:36 -0500

    first init

---
+++ js/shownews.js
@@ -0,0 +1,6 @@
+function showNews() {
+    var x = document.getElementById("hidden_news");
+    x.style.display = "block";
+    var x = document.getElementById("hide_news_botton");
+    x.style.display = "none";
+}
